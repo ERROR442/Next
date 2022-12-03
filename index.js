abfd99d69e80692eb5c0b4ce3187f558158edb23
ERRORE442 <ferraragiovanni18082008@gmail.com>
2022-12-03 19:11:55 +0100

    upload files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
 
-client.login("MTA0ODYwOTM2MDYxMzQ5MDY4OQ.GpqOoz.xBydWsoVaXJtO0_CzLuH8wdg3uzTYVa44ttYJo")
+client.login("process.env.token")
